88d59fef5f405a048d42519bad6c44181712c969
ZhukMV <133659486+ZhukMV@users.noreply.github.com>
2023-05-17 15:30:51 +0300

    Update 09_task_tuple.py

--- Module4/practice/02_list_and_tuple/09_task_tuple.py
+++ Module4/practice/02_list_and_tuple/09_task_tuple.py
@@ -1,4 +1,30 @@
 # Дано 3 кортежа заполненные произвольными целыми числами.
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
 
-# TODO: your code here
+# Да отрубят мне руки за такой код, но как умею..
+
+import random
+
+tup1 = tuple([random.randint(1, 15) for _ in range(random.randint(1, 10))])
+tup2 = tuple([random.randint(1, 15) for _ in range(random.randint(1, 10))])
+tup3 = tuple([random.randint(1, 15) for _ in range(random.randint(1, 10))])
+
+print(*tup1, "\n", *tup2, "\n", *tup3)
+
+minimal_tuple = []
+min_tuple_lens = min(len(tup1), len(tup2), len(tup3))
+
+count = 0
+
+if len(tup1) == min_tuple_lens:
+    minimal_tuple.extend(tup1)
+elif len(tup2) == min_tuple_lens:
+    minimal_tuple.extend(tup2)
+else:
+    minimal_tuple.extend(tup3)
+
+for number in minimal_tuple:
+    if number in tup1 and number in tup2 and number in tup3:
+        count += 1
+
+print(min_tuple_lens, *minimal_tuple, count)
